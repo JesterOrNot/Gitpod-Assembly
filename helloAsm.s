b56ac4bc2b7c691ba7e14a8680d93cc19f31023d
Sean Hellum <seanhellum45@gmail.com>
2020-02-26 01:40:07 +0000

    update

--- helloAsm.s
+++ helloAsm.s
@@ -1,6 +1,5 @@
-        .global _start
+.global _start
 
-        .text
 _start:
     # write(1, message, 13)
     mov     $1, %rax                # system call 1 is write
@@ -18,7 +17,6 @@ _start:
     mov     $60, %rax               # system call 60 is exit
     xor     %rdi, %rdi              # we want return code 0
     syscall                         # invoke operating system to exit
-message:
-	.ascii  "Hello world from x86_64 Assembly\n"
-hello:
-	.ascii  "Hello\n"
+
+message: .ascii  "Hello world from x86_64 Assembly\n"
+hello: .ascii  "Hello\n"
